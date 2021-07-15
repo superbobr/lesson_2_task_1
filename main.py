77d26e8da707146902f3ad96347a56086e42328b
superbobr <42520236+superbobr@users.noreply.github.com>
2021-07-15 11:02:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+HELP = '''
+help - напечатать справку по программе
+add - добавить задачу в список
+show - напечатать все добавленные задачи'''
+
+tasks = []
+
+run = True
+
+while run:
+  command = input('Введите команду: ')
+  if command == 'help':
+    print(HELP)
+  elif command == 'show':
+    print(tasks)
+  elif command == 'add':
+    task = input('Введите название задачи: ')
+    tasks.append(task)
+    print('Задача добавлена')
+  elif command == 'exit':
+    print('Спасибо за использование! До свидания!')
+    run = False
+  else:
+    print('Неизвестная команда')
+    print('До свидания!')
+    run = False
+
